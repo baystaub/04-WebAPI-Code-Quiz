21a109e4724fee74859837c97a54682a29866540
Ducky <staubbm@gmail.com>
2021-03-15 12:34:50 -0700

    got questions in the js, timer is working correctly now, questions and answers are not showing on the buttones needed too

--- assests/logic.js
+++ assests/logic.js
@@ -1,30 +1,65 @@
 //Sets start button as a variable
-var Startbtn = $('button')
+var Startbtn = $('#Button')
+var questionContainerElement = $("#questionContainer");
+var Highscores = $('#Highscores');
+var questionEl = $("#question")
     //gets time left Id
 var timeLeft = document.getElementById('Tl');
 var secondsLeft = '';
 var score = '';
 var confirm = '';
-
+var answerButtons = $("#answers");
+let question = [{
+    question: 'What laungage do you use for logic based coding?',
+    answers: [
+        { text: 'JavaScript', correct: true },
+        { text: 'CSS', correct: false },
+        { text: 'HTML', correct: false },
+        { text: 'ZOOM', correct: false }
+    ]
+}, {
+    question: 'What would you use in css to make font bold?',
+    answers: [
+        { text: 'Font-weight', correct: true },
+        { text: 'Text-content', correct: false },
+        { text: 'Text-type', correct: false },
+        { text: 'Your imagination', correct: false }
+    ]
+}, {
+    question: 'why would you style in javascript instead of css?',
+    answers: [
+        { text: 'just cause', correct: false },
+        { text: 'dynamicly changing things', correct: true },
+        { text: 'for fun', correct: false },
+        { text: 'for the extra challenge', correct: false }
+    ]
+}];
 
 //on click with the start button
-Startbtn.on('click', function(event) {
-    event.preventDefault();
-    console.log('clicked initial')
-        //sets second left in the timer bar
-    secondsLeft = 80;
-    Hide();
+Startbtn.on('click', startGame);
+
+function startGame() {
+    //hides the start button
+    Startbtn.addClass('hide1');
+    //hides the highscoores list
+    Highscores.addClass("hide1");
+    //shows the question container
+    document.getElementById('questionContainer').style.display = 'grid'
+        //shows the timer
+    document.getElementById('timer').style.display = 'grid'
+        //randomly shuffles the question
+    shuffledQuestions = question.sort(() => Math.random() - 1);
+    //starting the question index off at 0
+    currentQuestionIndex = 0;
+    //console logs the initial click to start the game
+    console.log('clicked initial');
+    //sets second left in the timer bar
+    secondsLeft = 10000000000;
     //starts timer function when clicked, counts down from 80
     setTime();
-    var question1 = document.getElementById('question-1').style.display = 'grid'
-});
-
-answer1.on('click', function(event) {
-    event.preventDefault();
-    document.getElementById('question-1').style.display = 'none'
-    document.getElementById('question-2').style.display = 'grid'
-})
-
+    reset();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
 
 //set time function
 function setTime() {
@@ -33,19 +68,47 @@ function setTime() {
         timeLeft.textContent = secondsLeft;
 
         if (secondsLeft === 0) {
+            //clears the time interval to stop counting
             clearInterval(timerInterval);
+            //alerts player to try again
             alert('please try again');
-            document.getElementById('button').style.display = '';
+            //when time is up shows the start button to try again
+            Startbtn.removeClass('hide1');
+            //calls function to hide
+            hideQuestions();
         }
 
     }, 1000);
+
 };
 
-//hides the main screen function
-function Hide() {
-    //sets everything besides title to display none
-    document.getElementById("Highscores").style.display = "none";
-    document.getElementById('button').style.display = "none";
-    //sets the timer to display when button is clicked
-    document.getElementById('timer').style.display = 'block'
-};
+function hideQuestions() {
+    //sets question container to display none
+    questionContainerElement.addClass('hide1');
+}
+
+function selectAnswer(e) {
+
+}
+
+function showQuestion(question) {
+    questionEl.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtons.append('button');
+        console.log(question);
+    })
+}
+
+function reset() {
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild;
+        (answerButtons.firstChild);
+    }
+}
