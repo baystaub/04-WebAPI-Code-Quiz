9cc29d1be2f8b2206f91b73ed74292a5e2d26dac
Ducky <staubbm@gmail.com>
2021-03-15 10:17:50 -0700

    got the highscore to hide and start button to hide and come back up. question 1 appers but resets timer when you hit any of the buttons

--- assests/logic.js
+++ assests/logic.js
@@ -2,33 +2,31 @@
 var Startbtn = $('button')
     //gets time left Id
 var timeLeft = document.getElementById('Tl');
-var secondsLeft = 80;
+var secondsLeft = '';
+var score = '';
+var confirm = '';
+
+
 //on click with the start button
-Startbtn.on('click', function() {
-    //sets second left in the timer bar
-    secondsLeft = 10;
-    //sets everything besides title to display none
-    document.getElementById("Highscores").style.display = "none";
-    document.getElementById('button').style.display = 'none';
-    //sets the timer to display when button is clicked
-    document.getElementById('timer').style.display = 'block'
-        //starts timer function when clicked, counts down from 80
-    setTime()
-    if (Question1 === true) {
-        if (Question2 === true) {
-            if (Question3 === true) {
-                prompt('Please enter your name for the score of' + timeLeft)
-            }
-        }
-    }
+Startbtn.on('click', function(event) {
+    event.preventDefault();
+    console.log('clicked initial')
+        //sets second left in the timer bar
+    secondsLeft = 80;
+    Hide();
+    //starts timer function when clicked, counts down from 80
+    setTime();
+    var question1 = document.getElementById('question-1').style.display = 'grid'
 });
 
+answer1.on('click', function(event) {
+    event.preventDefault();
+    document.getElementById('question-1').style.display = 'none'
+    document.getElementById('question-2').style.display = 'grid'
+})
 
 
-function Quiz() {
-    alert('you suck at this game');
-}
-
+//set time function
 function setTime() {
     var timerInterval = setInterval(function() {
         secondsLeft--;
@@ -41,41 +39,13 @@ function setTime() {
         }
 
     }, 1000);
-}
+};
 
-
-function Question1() {
-
-}
-
-function Question2() {
-
-}
-
-function Question3() {
-
-}
-
-
-
-//question 1
-//What launguage do you use for logic based coding?
-//Javascript  vv
-//css
-//html
-//Zoom
-
-
-//question 2
-//what would you use in css to make font bold?
-//font-weight  vv
-//text-content
-//text-type
-//font-style
-
-//question 3
-//Why would you change style in javascript instead of css?
-//just cause?
-//dynamicly changing? vv
-//for fun
-//for the extra challenge?
+//hides the main screen function
+function Hide() {
+    //sets everything besides title to display none
+    document.getElementById("Highscores").style.display = "none";
+    document.getElementById('button').style.display = "none";
+    //sets the timer to display when button is clicked
+    document.getElementById('timer').style.display = 'block'
+};
