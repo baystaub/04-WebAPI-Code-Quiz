84320469ef3f8f21bf90016bd02c9ef7f41f3448
Ducky <staubbm@gmail.com>
2021-03-17 13:32:53 -0700

    questions move from one to the next, at the end of the last question congragulates you and prompts you to put in highscore name

--- assests/logic.js
+++ assests/logic.js
@@ -8,50 +8,13 @@ var timeLeft = document.getElementById('Tl');
 var secondsLeft = '';
 var score = '';
 var confirm = '';
-var answerButtons = $("#answers");
-const questions = [{
-    question: 'What laungage do you use for logic based coding?',
-    answers: [
-        { text: 'JavaScript', correct: true },
-        { text: 'CSS', correct: false },
-        { text: 'HTML', correct: false },
-        { text: 'ZOOM', correct: false }
-    ]
-}, {
-    question: 'What would you use in css to make font bold?',
-    answers: [
-        { text: 'Font-weight', correct: true },
-        { text: 'Text-content', correct: false },
-        { text: 'Text-type', correct: false },
-        { text: 'Your imagination', correct: false }
-    ]
-}, {
-    question: 'why would you style in javascript instead of css?',
-    answers: [
-        { text: 'just cause', correct: false },
-        { text: 'dynamicly changing things', correct: true },
-        { text: 'for fun', correct: false },
-        { text: 'for the extra challenge', correct: false }
-    ]
-}];
-let shuffledQuestions, currentQuestionIndex
-var answer = $('button');
-
+var question1 = $("#question1");
+var question2 = $("#question2");
+var question3 = $("#question3");
 
 //on click with the start button
-Startbtn.on('click', startGame); {
-    answer.on('click', function() {
-        if ('data-correct' === true) {
-            console.log('clicked')
-            currentQuestionIndex++
-            reset();
-            setNextQuestion();
-        } else {
-            alert('wrong answer please pick a differnt question');
-            console.log('wrong')
-        }
-    })
-};
+Startbtn.on('click', startGame);
+
 
 function startGame() {
     //hides the start button
@@ -62,24 +25,15 @@ function startGame() {
     document.getElementById('questionContainer').style.display = 'grid'
         //shows the timer
     document.getElementById('timer').style.display = 'grid'
-        //randomly shuffles the question
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
-    //starting the question index off at 0
-    currentQuestionIndex = 0;
-    //console logs the initial click to start the game
+        //console logs the initial click to start the game
     console.log('start game');
     //sets second left in the timer bar
-    secondsLeft = 5;
+    secondsLeft = 10;
     //starts timer function when clicked, counts down from 80
     setTime();
-    setNextQuestion();
-    questionContainerElement.removeClass('hide1')
 }
 
-function setNextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-    reset();
-};
+
 
 //set time function
 function setTime() {
@@ -95,66 +49,38 @@ function setTime() {
             //when time is up shows the start button to try again
             Startbtn.removeClass('hide1');
             Highscores.removeClass('hide1');
-            //calls function to hide
-            hideQuestions();
+            questionContainerElement.addClass('hide1');
         }
 
     }, 1000);
 
 };
 
-function hideQuestions() {
-    //sets question container to display none
-    questionContainerElement.addClass('hide1');
+function clickedq1() {
+    console.log('clicked');
+    question1.addClass('hide');
+    question2.removeClass('hide');
 };
 
-function selectAnswer(e) {
-    let selected = e.target;
-    var correct = selected.dataset.correct;
-    setStatusClass(document.body.button, correct)
-    Array.from(answerButtons.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-
-    if (shuffledQuestions.length < currentQuestionIndex + 1) {
-        var high = prompt('congradulation you completed the quiz, Please eneter your name;' + timeLeft)
-        console.log(high)
-    };
-}
-
-function setStatusClass(element, correct) {
-    clearStatus(element)
-    if (correct) {
-        $(element).addClass('correct')
-    } else {
-
-        $(element).addClass('wrong')
-    }
+function clickedq2() {
+    console.log('clicked2');
+    question2.addClass('hide');
+    question3.removeClass('hide');
 };
 
-function clearStatus(element) {
-    $(element).removeClass('correct')
-    $(element).removeClass('wrong')
+function clickedq3() {
+    console.log('clicked3');
+    question3.addClass('hide');
+    Highscores.removeClass("hide1")
+    alert('Congrats your completed the quiz!');
+    var name = prompt('please enter your name for your highscore!')
+    console.log(name + ':' + secondsLeft);
+    highscore();
+
 };
 
-function showQuestion(questions) {
-    questionEl.text(questions.question);
-    console.log(questions)
-    questions.answers.forEach(answer => {
-        let button = document.createElement("button");
-        button.innerText = answer.text;
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtons.append(button);
-        console.log(question);
-    })
-};
+function highscore() {
 
-function reset() {
-    while (answerButtons.firstChild) {
-        answerButtons.removeChild;
-        (answerButtons.firstChild);
-    }
+
+
 }
